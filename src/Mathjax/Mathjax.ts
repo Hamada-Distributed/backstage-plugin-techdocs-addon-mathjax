31e1a6e798bff2d8cfa2ed81745d5b336c9d749c
Hamada Gasmallah <mgasmallah@hotmail.com>
2023-04-01 22:01:51 -0400

    fix: make sure to load mathjax script if not loaded

--- src/Mathjax/Mathjax.ts
+++ src/Mathjax/Mathjax.ts
@@ -1,9 +1,27 @@
 import { useEffect } from 'react';
 
+function runTypeSet(){
+    if (typeof (<any>window)?.MathJax !== "undefined"){
+      console.log("MathJax was found and typeset is called!");
+      (<any>window).MathJax.typeset();
+      return true;
+    }
+    return false;
+};
+
+
 export const MathjaxAddon = () => {  
   useEffect(()=>{
-    if (typeof (<any>window)?.MathJax !== "undefined"){
-      (<any>window).MathJax.typeset();
+
+    if (!runTypeSet()){
+      console.log("No MathJax was found, now loading!");
+      const script = document.createElement('script');
+      script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js';
+      script.async = true;
+      document.body.appendChild(script);
+      setTimeout(()=>{
+        runTypeSet();      
+      }, 500);
     };
 
   }, []);
